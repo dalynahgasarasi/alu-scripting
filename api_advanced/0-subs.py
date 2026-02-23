adbfc15e14dd64e9f9a135f22dd8ea9d21915c7d
dalynahgasarasi <d.gasarasi@alustudent.com>
2026-02-23 12:58:30 -0800

    Modified

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "python:alu.api.advanced:v1.0 (by /u/dalynahgasarasi)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
