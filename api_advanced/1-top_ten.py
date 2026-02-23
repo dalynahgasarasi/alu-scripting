0849409f4744a4736153910f7cef844c90007372
dalynahgasarasi <d.gasarasi@alustudent.com>
2026-02-23 12:48:25 -0800

    Add 10 functions

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that prints the top 10 hot posts for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the first 10 hot posts for a subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json().get("data", {}).get("children", [])
+    for post in posts:
+        print(post.get("data", {}).get("title"))
